09386bf5b96af63dbd8b291eadfce61bb6879821
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-24 18:02:49 -0600

    Create files for planned classes

---
+++ test/offset_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/offset'
+require 'date'
+
+class OffsetTest < Minitest::Test
+  def setup
+  end
+end
