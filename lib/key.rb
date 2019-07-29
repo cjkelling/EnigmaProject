71a3a7826896785dc9d6938e76260fc37367a451
Christopher Kelling <cj.kelling88@gmail.com>
2019-07-29 11:17:49 -0600

    Decrypt works

--- lib/key.rb
+++ lib/key.rb
@@ -1,8 +1,8 @@
 class Key
-  attr_reader :num, :keys, :a_key, :b_key, :c_key, :d_key
+  attr_reader :num, :key, :a_key, :b_key, :c_key, :d_key
 
-  def initialize(keys = 0)
-    @keys = keys
+  def initialize(key = 0)
+    @key = key
     @num = []
   end
 
@@ -12,16 +12,16 @@ class Key
   end
 
   def assign_values
-    if @keys == 0
+    if @key == 0
       @a_key = (@num[0..1]).join
       @b_key = (@num[1..2]).join
       @c_key = (@num[2..3]).join
       @d_key = (@num[3..4]).join
     else
-      @a_key = (@keys[0..1])
-      @b_key = (@keys[1..2])
-      @c_key = (@keys[2..3])
-      @d_key = (@keys[3..4])
+      @a_key = (@key[0..1])
+      @b_key = (@key[1..2])
+      @c_key = (@key[2..3])
+      @d_key = (@key[3..4])
     end
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -14,7 +14,7 @@ class EnigmaTest < Minitest::Test
     @key = Key.new('02715')
     @offset = Offset.new('040895')
     @shift = Shift.new
-    @message = Message.new('hello world')
+    @message = Message.new
     @key.assign_values
     @offset.date_squared
     @offset.last_four
@@ -23,9 +23,6 @@ class EnigmaTest < Minitest::Test
       @key.a_key, @offset.a_offset, @key.b_key, @offset.b_offset,
       @key.c_key, @offset.c_offset, @key.d_key, @offset.d_offset
     )
-    @message.split_message
-    @message.new_letters(@shift.a_shift, @shift.b_shift, @shift.c_shift, @shift.d_shift)
-    @message.recombine_to_ciphertext
   end
 
   def test_it_exists
@@ -33,26 +30,26 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt
+    @message.split_message('hello world')
+    @message.new_letters(@shift.a_shift, @shift.b_shift, @shift.c_shift, @shift.d_shift)
+    @message.recombine_to_ciphertext
     expected = {
       encryption: 'keder ohulw',
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.encrypt(@message, @key.keys, @offset.date)
+    assert_equal expected, @enigma.encrypt(@message, @key.key, @offset.date)
   end
-  #
-  # def test_decrypt
-  #   expected = {
-  #     encryption: 'hello world',
-  #     key: '02715',
-  #     date: '040895'
-  #   }
-  #   assert_equal expected, @enigma.decrypt('keder ohulw', '02715', '040895')
-  # end
 
-  # def test_encrypted
-  #   encrypted = enigma.encrypt("hello world", "02715")
-  #   enigma.decrypt(encrypted[:encryption], "02715")
-  #   enigma.encrypt("hello world")
-  # end
+  def test_decrypt
+    @message.split_message('keder ohulw')
+    @message.new_letters_reverse(@shift.a_shift, @shift.b_shift, @shift.c_shift, @shift.d_shift)
+    @message.recombine_to_ciphertext
+    expected = {
+      decryption: 'hello world',
+      key: '02715',
+      date: '040895'
+    }
+    assert_equal expected, @enigma.decrypt(@message, @key.key, @offset.date)
+  end
 end
